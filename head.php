2bc9facab264d93ba775c15027723903240ffab1
Eric Johansson <johanssoneric5@gmail.com>
2016-12-05 18:25:37 +0900

    test commit

--- head.php
+++ head.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Japanese verbs</title>
+    <title>Japanese verb game</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
